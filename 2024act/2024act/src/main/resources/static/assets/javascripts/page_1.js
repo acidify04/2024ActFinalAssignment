17cb566b2c90fb37fc212a5e0a8d7fbde74a9a08
SunJiHoon <cangogi1@naver.com>
2024-06-21 19:42:36 +0900

    토글 버튼 예쁘게 수정

--- 2024act/2024act/src/main/resources/static/assets/javascripts/page_1.js
+++ 2024act/2024act/src/main/resources/static/assets/javascripts/page_1.js
@@ -4,11 +4,11 @@ document.querySelectorAll('.toggleButton').forEach(button => {
         if (details.classList.contains('hidden')) {
             details.classList.remove('hidden');
             details.style.display = 'block';
-            this.textContent = 'Hide Details';
+            // this.textContent = 'Hide Details';
         } else {
             details.classList.add('hidden');
             details.style.display = 'none';
-            this.textContent = 'Show Details';
+            // this.textContent = 'Show Details';
         }
     });
 });
