ce708cee4fd60608cdfd9eebc1ffc49354c1d4f6
acidify04 <parkeh0724@gmail.com>
2024-06-22 19:04:43 +0900

    GptController 개발

--- 2024act/2024act/src/main/java/com/example/_act/controller/GptController.java
+++ 2024act/2024act/src/main/java/com/example/_act/controller/GptController.java
@@ -2,12 +2,19 @@ package com.example._act.controller;
 
 import com.example._act.service.GptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/gpt/")
 public class GptController {
     @Autowired
     GptService gptService;
 
-
+    @GetMapping("/ask/{question}")
+    public String askController(@RequestParam(name = "question") String question) {
+        return gptService.askQuestion(question);
+    }
 }
