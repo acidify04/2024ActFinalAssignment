55c4e71e6f97e67eef6a156c3e8030776ad828de
acidify04 <parkeh0724@gmail.com>
2024-06-22 19:08:46 +0900

    로직 에러 수정

--- 2024act/2024act/src/main/java/com/example/_act/controller/GptController.java
+++ 2024act/2024act/src/main/java/com/example/_act/controller/GptController.java
@@ -2,10 +2,7 @@ package com.example._act.controller;
 
 import com.example._act.service.GptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/gpt/")
@@ -14,7 +11,8 @@ public class GptController {
     GptService gptService;
 
     @GetMapping("/ask/{question}")
-    public String askController(@RequestParam(name = "question") String question) {
+    @ResponseBody
+    public String askController(@PathVariable(name = "question") String question) {
         return gptService.askQuestion(question);
     }
 }
