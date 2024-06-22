464d8bdbab42e967e0e15ee4c8c05a54b8705cbd
acidify04 <parkeh0724@gmail.com>
2024-06-22 17:48:24 +0900

    GptController 세팅

--- 2024act/2024act/src/main/java/com/example/_act/controller/GptController.java
+++ 2024act/2024act/src/main/java/com/example/_act/controller/GptController.java
@@ -1,4 +1,13 @@
 package com.example._act.controller;
 
+import com.example._act.service.GptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GptController {
+    @Autowired
+    GptService gptService;
+
+
 }
